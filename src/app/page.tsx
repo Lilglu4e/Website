94e6fac2cf829ebd11cfc211a077a8359d9b0eb3
Lilglu4e <mohammadsarrafmstr2000@gmail.com>
2023-09-10 18:07:30 -0400

    npm run build fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -37,17 +37,13 @@ export default function Home() {
 							Developer and designer.
 						</h3>
 						<p className="text-md py-5 leading-8 text-gray-800 dark:text-gray-200 max-w-xl mx-auto md:text-xl">
-							Freelancer providing services for programming and design content
-							needs. Join me down below and let's get cracking!
+							{`Freelancer providing services for programming and design content
+							needs. Join me down below and let's get cracking!`}
 						</p>
 						<div className="text-5xl flex justify-center gap-16 py-3 text-gray-600 dark:text-gray-400">
 							<AiFillGithub />
 							<AiFillLinkedin />
-							{/* <AiFillYoutube /> */}
 						</div>
-						{/* <div className="mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 relative overflow-hidden mt-20 md:h-96 md:w-96"> */}
-							{/* <Image src={deved} layout="fill" objectFit="cover" /> */}
-						{/* </div> */}
 					</div>
 				</section>
 			</main>
