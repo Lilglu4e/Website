1fad86adf52a1f4980ec171876e5f443a18dad36
Lilglu4e <mohammadsarrafmstr2000@gmail.com>
2023-09-26 02:28:23 -0400

    projects added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,13 @@ import React, { useState } from 'react';
 import { BsFillMoonStarsFill, BsSun } from 'react-icons/bs';
 import { AiFillGithub, AiFillLinkedin, AiFillYoutube } from 'react-icons/ai';
 import { IoLibraryOutline } from 'react-icons/io5';
-import {PiFrameCornersBold} from 'react-icons/pi'
-import {FaCode} from 'react-icons/fa6'
+import { PiFrameCornersBold } from 'react-icons/pi';
+import { FaCode } from 'react-icons/fa6';
+import { SiNetlify } from 'react-icons/si';
 import Link from 'next/link';
 import Head from 'next/head';
+import Image from 'next/image';
+import web1 from '../../public/nexxt-tic.png'
 export default function Home() {
   const [darkMode, setDarkMode] = useState(true);
 
@@ -84,7 +87,7 @@ export default function Home() {
           </div>
           <div className="lg:flex gap-10">
             <div className="text-center dark:text-white shadow-lg p-10 rounded-xl my-10 bg-gradient-to-t from-gray-300 to-gray-100 dark:bg-gradient-to-t dark:from-gray-700 dark:to-gray-800 flex-1">
-              <PiFrameCornersBold className={`w-16 h-16 text-teal-600 mx-auto`}/>
+              <PiFrameCornersBold className={`w-16 h-16 text-teal-600 mx-auto`} />
               <h3 className="text-lg font-medium pt-8 pb-2  ">
                 FrameWorks
               </h3>
@@ -101,7 +104,7 @@ export default function Home() {
             </div>
             <div className="text-center dark:text-white shadow-lg p-10 rounded-xl my-10 bg-gradient-to-t from-gray-300 to-gray-100 dark:bg-gradient-to-t dark:from-gray-700 dark:to-gray-800 flex-1">
               {/* <Image src={code} width={100} height={100} /> */}
-              <IoLibraryOutline className={`w-16 h-16 text-teal-600 mx-auto`}/>
+              <IoLibraryOutline className={`w-16 h-16 text-teal-600 mx-auto`} />
               <h3 className="text-lg font-medium pt-8 pb-2 ">
                 Libraries
               </h3>
@@ -117,7 +120,7 @@ export default function Home() {
               <p className="dark:text-white py-1">matplotlib</p>
             </div>
             <div className="text-center dark:text-white shadow-lg p-10 rounded-xl my-10 bg-gradient-to-t from-gray-300 to-gray-100 dark:bg-gradient-to-t dark:from-gray-700 dark:to-gray-800 flex-1">
-              <FaCode className={`w-16 h-16 text-teal-600 mx-auto`}/>
+              <FaCode className={`w-16 h-16 text-teal-600 mx-auto`} />
               <h3 className="text-lg font-medium pt-8 pb-2 ">Languages</h3>
               <p className="py-2">
                 ### ######  ###### # ####  # #####  ### # # # ## # # #
@@ -130,6 +133,67 @@ export default function Home() {
               <p className="dark:text-white py-1">HTML/CSS/JavaScript</p>
               <p className="dark:text-white py-1">Java</p>
             </div>
+          </div>
+        </section>
+        <section className="py-10">
+          <div>
+            <h3 className="text-3xl py-1 dark:text-white ">Portofolio</h3>
+            <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
+              Since the beginning of my journey as a freelance designer and
+              developer, I've done remote work for
+              <span className="text-teal-500"> agencies </span>
+              consulted for <span className="text-teal-500">startups </span>
+              and collaborated with talanted people to create digital products
+              for both business and consumer use.
+            </p>
+            <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
+              I offer from a wide range of services, including brand design,
+              programming and teaching.
+            </p>
+          </div>
+          <div className="flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap">
+            <div className="basis-1/3 flex-1 relative group">
+              <Image
+                className="rounded-lg object-cover"
+                layout="responsive"
+                src={web1}
+                alt={``}
+              />
+              <div className='bg-gray-500 opacity-0 group-hover:opacity-100 absolute inset-x-0 top-0 p-4 transform translate-y-full transition-opacity duration-300'>
+                <div className='flex justify-between items-center'>
+                  <p className='text-white ml-3 mt-3'>Tic-Tac-Toe</p>
+                </div>
+                <div className='flex justify-between items-center pb-1'>
+                  <p className='text-white ml-3'>Next.js, React, HTML, JavaScript, CSS, Tailwind, Firebase</p>
+                  <div className='flex items-center'>
+                    <Link href={'https://github.com/Lilglu4e/next_tic'}><AiFillGithub className='text-white mr-3 w-6 h-6' /></Link>
+                    <Link href={'https://lilglu4e-firebase.netlify.app/'}><SiNetlify className='text-white w-6 h-6 mr-3' /></Link>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="basis-1/3 flex-1 relative group">
+              <Image
+                className="rounded-lg object-cover"
+                layout="responsive"
+                src={web1}
+                alt={``}
+              />
+              <div className='bg-gray-500 opacity-0 group-hover:opacity-100 absolute inset-x-0 top-0 p-4 transform translate-y-full transition-opacity duration-300'>
+                <div className='flex justify-between items-center'>
+                  <p className='text-white ml-3 mt-3'>Tic-Tac-Toe</p>
+                </div>
+                <div className='flex justify-between items-center pb-1'>
+                  <p className='text-white ml-3'>Next.js, React, HTML, JavaScript, CSS, Tailwind, Firebase</p>
+                  <div className='flex items-center'>
+                    <Link href={'https://github.com/Lilglu4e/next_tic'}><AiFillGithub className='text-white mr-3 w-6 h-6' /></Link>
+                    <Link href={'https://lilglu4e-firebase.netlify.app/'}><SiNetlify className='text-white w-6 h-6 mr-3' /></Link>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+
           </div>
         </section>
       </main>
